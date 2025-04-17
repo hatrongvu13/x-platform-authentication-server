b65592f71a245a1b669e0d2f46dbaaa8880d35e5
hatrongvu13 <vuht7@msb.com.vn>
2025-04-17 17:47:53 +0700

    update code build registered client

--- src/main/java/com/xxx/authentication/server/config/OAuth2ClientProperties.java
+++ src/main/java/com/xxx/authentication/server/config/OAuth2ClientProperties.java
@@ -1,21 +1,19 @@
 package com.xxx.authentication.server.config;
 
 import lombok.Data;
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 @ConfigurationProperties(prefix = "oauth2")
 @Component
 @Data
 public class OAuth2ClientProperties {
     private List<Client> clients = new ArrayList<>();
-
-    public List<Client> getClients() {
-        return clients;
-    }
 
     @Data
     public static class Client {
--- src/main/java/com/xxx/authentication/server/config/SecurityConfig.java
+++ src/main/java/com/xxx/authentication/server/config/SecurityConfig.java
@@ -16,13 +16,10 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
-import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
-import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
@@ -34,101 +31,12 @@ import java.security.interfaces.RSAPublicKey;
 import java.util.List;
 import java.util.UUID;
 
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    @Order(1)
-//    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
-//        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer = new OAuth2AuthorizationServerConfigurer();
-//        http.securityMatcher("/oauth2/**", "/.well-known/**", "/oidc/**") // Chỉ áp dụng cho các endpoint OAuth2
-//                .with(authorizationServerConfigurer, authConfigurer ->
-//                        authConfigurer
-//                                .oidc(Customizer.withDefaults()) // Kích hoạt OIDC
-//                                .authorizationEndpoint(Customizer.withDefaults())
-//                                .tokenEndpoint(Customizer.withDefaults())
-//                )
-//                .exceptionHandling(exceptions -> exceptions
-//                        .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
-//                )
-//                .csrf(csrf -> csrf.ignoringRequestMatchers("/oauth2/**", "/.well-known/**", "/oidc/**"));
-//        return http.build();
-//    }
-//
-//    @Bean
-//    @Order(2)
-//    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .requestMatchers("/login").permitAll()
-//                        .anyRequest().authenticated()
-//                )
-//                .formLogin(form -> form.loginPage("/login").permitAll());
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        var user = User.withUsername("user")
-//                .password("{noop}password")
-//                .roles("USER")
-//                .build();
-//        return new InMemoryUserDetailsManager(user);
-//    }
-//
-//    @Bean
-//    public RegisteredClientRepository registeredClientRepository() {
-//        // Client cho Authorization Code
-//        RegisteredClient webClient = RegisteredClient.withId(UUID.randomUUID().toString())
-//                .clientId("web-client")
-//                .clientSecret("{noop}web-secret")
-//                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-//                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-//                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-//                .redirectUri("http://localhost:8080/login/oauth2/code/web-client")
-//                .scope(OidcScopes.OPENID)
-//                .scope(OidcScopes.PROFILE)
-//                .scope("api.read")
-//                .build();
-//
-//        // Client cho Client Credentials
-//        RegisteredClient serviceClient = RegisteredClient.withId(UUID.randomUUID().toString())
-//                .clientId("service-client")
-//                .clientSecret("{noop}service-secret")
-//                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-//                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-//                .scope("api.read")
-//                .build();
-//
-//        return new InMemoryRegisteredClientRepository(webClient, serviceClient);
-//    }
-//
-//    @Bean
-//    public JWKSource<SecurityContext> jwkSource() throws Exception {
-//        KeyPair keyPair = generateRsaKey();
-//        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-//        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-//        RSAKey rsaKey = new RSAKey.Builder(publicKey)
-//                .privateKey(privateKey)
-//                .keyID(UUID.randomUUID().toString())
-//                .build();
-//        JWKSet jwkSet = new JWKSet(rsaKey);
-//        return new ImmutableJWKSet(jwkSet);
-//    }
-//
-//    private static KeyPair generateRsaKey() throws Exception {
-//        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-//        keyPairGenerator.initialize(2048);
-//        return keyPairGenerator.generateKeyPair();
-//    }
-//}
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-private final OAuth2ClientProperties properties;
+    private final OAuth2ClientProperties properties;
+
     @Bean
     @Order(1)
     public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -185,23 +93,6 @@ private final OAuth2ClientProperties properties;
 
     @Bean
     public RegisteredClientRepository registeredClientRepository() {
-//        RegisteredClient webClient = RegisteredClient.withId("web-client-id")
-//                .clientId("web-client")
-//                .clientSecret("{noop}web-secret")
-//                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-//                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-//                .scope(OidcScopes.OPENID)
-//                .scope(OidcScopes.PROFILE)
-//                .scope("api.read")
-//                .build();
-//
-//        RegisteredClient serviceClient = RegisteredClient.withId("service-client-id")
-//                .clientId("service-client")
-//                .clientSecret("{noop}service-secret")
-//                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-//                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-//                .scope("api.read")
-//                .build();
 
         List<RegisteredClient> clients = properties.getClients().stream()
                 .map(prop -> {
@@ -212,9 +103,12 @@ private final OAuth2ClientProperties properties;
 
                     prop.getGrantTypes().forEach(grant -> {
                         switch (grant) {
-                            case "client_credentials" -> builder.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS);
-                            case "authorization_code" -> builder.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE);
-                            case "refresh_token" -> builder.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN);
+                            case "client_credentials" ->
+                                    builder.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS);
+                            case "authorization_code" ->
+                                    builder.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE);
+                            case "refresh_token" ->
+                                    builder.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN);
                             case "password" -> builder.authorizationGrantType(new AuthorizationGrantType("password"));
                             // Add other cases if needed
                         }
@@ -237,7 +131,7 @@ private final OAuth2ClientProperties properties;
                 .keyID("auth-server-key")
                 .build();
         JWKSet jwkSet = new JWKSet(rsaKey);
-        return new ImmutableJWKSet(jwkSet);
+        return new ImmutableJWKSet<>(jwkSet);
     }
 
     private static KeyPair generateRsaKey() throws Exception {
